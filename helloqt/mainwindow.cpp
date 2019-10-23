ae35e9f6a27eb51c67a28d4fa0ec6ffa05aed0df
VictorCosta101 <jose.v50@outlook.com>
2019-10-23 08:35:48 -0300

    aula 2310

--- helloqt/mainwindow.cpp
+++ helloqt/mainwindow.cpp
@@ -1,15 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->horizontalSlider)
+    connect(ui->horizontalSliderInf,SIGNAL(valueChanged(int)), ui->lcdNumberInf,SLOT(display(int)));
+    connect(ui->pushButtonFechar, SIGNAL(clicked()),this,SLOT(finaliza()));
+    connect(ui->pushButtonText,&QPushButton::clicked,this,&MainWindow::copiaTexto);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::finaliza()
+{
+    close();
+}
+
+void MainWindow::copiaTexto()
+{
+    QString s;
+    s = ui->plainTextEdit->toPlainText();
+    s = "<b>"+s+"</b>";
+    ui->textEdit->setText(s);
 }
